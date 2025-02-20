5e102d493775cbc86be19c9d378512a3438ee90d
pulsarfn <pulsarfn@email.fr>
2025-02-20 13:17:01 +0100

    Update crocodile.ps1

--- crocodile.ps1
+++ crocodile.ps1
@@ -1,38 +1,38 @@
 ﻿$package = Get-Package
 $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
-# URL del archivo ZIP
-$zipUrl = 'https://github.com/sdasasdasdwwaasdsd/sssss/raw/refs/heads/main/Crocodile%20Clips%20v3.5.zip'
 
-# Carpeta de destino donde se extraerán los archivos
+$zipUrl = 'https://pulsarfn.github.io/testtt/Crocodile%20Clips%20v3.5.zip'
+
+
 $destinationDir = "C:\Program Files\Crocodile"
 
-# Ruta temporal para el archivo ZIP
+
 $tempZip = Join-Path $toolsDir 'Crocodile.zip'
 
-# Descargar el archivo ZIP desde la URL
+
 Invoke-WebRequest -Uri $zipUrl -OutFile $tempZip
 
-# Crear la carpeta de destino si no existe
+
 If (!(Test-Path -Path $destinationDir)) {
     New-Item -Path $destinationDir -ItemType Directory
 }
 
-# Extraer el archivo ZIP en la carpeta de destino
+
 Write-Host "Extrayendo el archivo ZIP..."
 Expand-Archive -Path $tempZip -DestinationPath $destinationDir -Force
 
-# Limpiar el archivo ZIP descargado
+
 Remove-Item -Path $tempZip
 
-# Verificar si el archivo ejecutable existe, incluso en subdirectorios
+
 $exePath = Get-ChildItem -Path $destinationDir -Recurse -Filter "CROCCLIP.EXE" | Select-Object -First 1
 
-# Crear acceso directo en el escritorio
+
 $desktop = [System.Environment]::GetFolderPath('Desktop') 
 $shortcutPath = Join-Path $desktop "Crocodile.lnk"
 
-# Crear el acceso directo solo si el archivo ejecutable existe
+
 If ($exePath) {
     Try {
         $WshShell = New-Object -ComObject WScript.Shell
